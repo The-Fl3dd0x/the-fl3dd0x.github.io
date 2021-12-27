72e47ce22b20ae4e4d7efd79698edf05c8a440fb
Simon Lefering <simi2006@outlook.com>
2021-12-27 23:16:11 +0100

    add pause button

--- timer.js
+++ timer.js
@@ -1,16 +1,29 @@
 const second = 1000, minute = second * 60, hour = minute * 60;
-var startTime;
-var persecond;
-var dbg;
+var startTime, persecond, timer, distance, prevDistance = 0, isPaused = false;
 
 function setIntervalFun() {
-    let now = new Date().getTime(), distance = now - startTime;
+    var now = new Date().getTime();
+    distance = now - startTime + prevDistance;
 
     document.querySelector(".stunden").innerText = Math.floor(distance / hour);
     document.querySelector(".minuten").innerText = Math.floor((distance % hour) / minute);
     document.querySelector(".sekunden").innerText = Math.floor((distance % minute) / second);
 
     document.querySelector(".geldbetrag").innerText = ((distance / second) * persecond).toFixed(2);
+}
+
+function pause() {
+    if (isPaused) {
+        startTime = new Date().getTime();
+        timer = setInterval(setIntervalFun, 100);
+        isPaused = false;
+        document.querySelector(".btnPause").value = "Pause";
+    } else {
+        clearInterval(timer);
+        isPaused = true;
+        prevDistance = distance;
+        document.querySelector(".btnPause").value = "Weiter";
+    }
 }
 
 function getUrlVar(parameter) {
@@ -24,6 +37,6 @@ function getUrlVar(parameter) {
 
 window.onload = function() {
     startTime = new Date().getTime();
-    setInterval(setIntervalFun, 100);
+    timer = setInterval(setIntervalFun, 100);
     persecond = getUrlVar("persecond");
 };
